9dd7e2fcdb20b5dde16e17ca04cec021c5e7407d
Luqman <lqman104@gmail.com>
2023-08-12 14:17:49 +0700

    fix: form_container.dart elevation

--- lib/components/form_container.dart
+++ lib/components/form_container.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class FormContainer extends StatelessWidget {
-
   final List<Widget> children;
+
   const FormContainer({super.key, required this.children});
 
   @override
@@ -10,16 +10,22 @@ class FormContainer extends StatelessWidget {
     return Center(
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 24.0),
-        child: Container(
-          decoration: const BoxDecoration(
+        child: Material(
+          elevation: 8,
+          borderRadius: const BorderRadius.all(
+            Radius.circular(15.0),
+          ),
+          child: Container(
+            decoration: const BoxDecoration(
               borderRadius: BorderRadius.all(
                 Radius.circular(15.0),
               ),
               color: Colors.white,
-              boxShadow: [BoxShadow()]),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: children,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: children,
+            ),
           ),
         ),
       ),
