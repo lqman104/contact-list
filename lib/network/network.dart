e3bd136330ac73d0fc112ddc2d8af73985869112
Luqman <lqman104@gmail.com>
2023-08-13 15:15:52 +0700

    refactor: change to real url

--- lib/network/network.dart
+++ lib/network/network.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 
 final options = BaseOptions(
-  baseUrl: 'http://192.168.1.4:3001',
+  baseUrl: 'https://test-android.tongkolspace.com',
   connectTimeout: const Duration(seconds: 15),
   receiveTimeout: const Duration(seconds: 15),
 );
